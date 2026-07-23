69cc27871c4502f1d0bbeae519c6fadb7a03d339
hungnmph50619 <hungnmph50619@gmail.com>
2026-07-24 00:05:07 +0700

    fix: notify vehicle owner when a new reservation is created

--- Core/SmartCar.Application/Features/Mediator/Handlers/ReservationHandlers/CreateReservationCommandHandler.cs
+++ Core/SmartCar.Application/Features/Mediator/Handlers/ReservationHandlers/CreateReservationCommandHandler.cs
@@ -1,16 +1,23 @@
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SmartCar.Application.Features.Mediator.Commands.ReservationCommands;
 using SmartCar.Application.Interfaces;
 using SmartCar.Domain.BusinessRules;
 using SmartCar.Domain.Entities;
+using SmartCar.Persistence.Context;
 
 namespace SmartCar.Application.Features.Mediator.Handlers.ReservationHandlers
 {
     public class CreateReservationCommandHandler : IRequestHandler<CreateReservationCommand, int>
     {
         private readonly IRepository<Reservation> _repository;
+        private readonly CarBookContext _context;
 
-        public CreateReservationCommandHandler(IRepository<Reservation> repository) => _repository = repository;
+        public CreateReservationCommandHandler(IRepository<Reservation> repository, CarBookContext context)
+        {
+            _repository = repository;
+            _context = context;
+        }
 
         public async Task<int> Handle(CreateReservationCommand request, CancellationToken cancellationToken)
         {
@@ -58,6 +65,25 @@ namespace SmartCar.Application.Features.Mediator.Handlers.ReservationHandlers
             };
 
             await _repository.CreateAsync(reservation);
+
+            var ownerUserId = await _context.PartnerVehicles
+                .AsNoTracking()
+                .Where(x => x.PartnerVehicleID == request.PartnerVehicleID)
+                .Select(x => x.OwnerAppUserID)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (ownerUserId > 0)
+            {
+                _context.Notifications.Add(new Notification
+                {
+                    AppUserID = ownerUserId,
+                    Title = "Có yêu cầu thuê xe mới",
+                    Message = $"Đơn #{reservation.ReservationID} đang chờ bạn phản hồi. Thời gian nhận xe: {request.PickUpDate:dd/MM/yyyy} {request.PickUpTime:hh\\:mm}.",
+                    Type = "Reservation"
+                });
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+
             return reservation.ReservationID;
         }
     }
